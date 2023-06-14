2aac2fe0bc72e21dfa9691b30792946126041569
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-14 22:26:19 +0100

    imports

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,5 @@
+export { default as Link } from "next/link";
 export { default as Image } from "next/image";
+export { default as CustomButton } from "./CustomButton";
+export { default as Navbar } from './Navbar'
+export { default as Footer } from "./Footer"; 
