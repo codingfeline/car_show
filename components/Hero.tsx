e9e3e149a5b668d9556044637903a850706746b4
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-14 22:24:16 +0100

    layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,29 @@
-import { Image } from '@components/index'
+'use client'
+
+import { CustomButton, Image } from '@components/index'
 
 const Hero = () => {
+  const handleScroll = () => {}
   return (
-    <div>
-      <h2>Hero</h2>
-      <Image src={'/vercel.svg'} width={50} height={50} alt="Hero image" />
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Find, book, or rent a car -- quickly and easily.</h1>
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking process.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+        {/* <div className="hero__image-overlay" /> */}
+      </div>
     </div>
   )
 }
