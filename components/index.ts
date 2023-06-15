fed1f63e5baee2a3fc65728744680d6a4b2405d0
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-15 22:28:19 +0100

    imports

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,5 @@ export { default as Link } from "next/link";
 export { default as Image } from "next/image";
 export { default as CustomButton } from "./CustomButton";
 export { default as Navbar } from './Navbar'
-export { default as Footer } from "./Footer"; 
+export { default as Footer } from "./Footer";
+export { footerLinks } from "@constants";
